3914849ee016338b0ad6ea8a70114a9b65f9c6f1
raphaelpg <raphael_pinto@outlook.com>
2019-10-24 16:21:32 +0200

    initialisation

---
+++ defi1.js
@@ -0,0 +1,29 @@
+//Fonction qui converti un hexadécimal -> décimal
+function convertHexaToDeci(string) {
+
+}
+
+//Fonction qui converti un décimal -> hexadécimal
+function convertDeciToHexa(number){
+
+}
+
+//Fonction qui converti un hexadécimal little endian -> hexadécimal
+function convertHexaLittleEndianToHexa(string) {
+
+}
+
+//Fonction qui converti un varInt -> décimal
+function convertVarIntToDeci(string) {
+
+}
+
+//Fonction qui converti un varInt -> décimal
+function convertBitsToTarget(number) {
+
+}
+
+//Fonction qui converti un varInt -> décimal
+function convertTargetToDifficulty(number) {
+
+}
