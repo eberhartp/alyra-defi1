71dc7c5a7aa75e0872a22c46efe27aa7b54a7e97
raphaelpg <raphael_pinto@outlook.com>
2019-10-25 17:48:32 +0200

    insert function 4

--- defi1.js
+++ defi1.js
@@ -14,8 +14,30 @@ function convertHexaLittleEndianToHexa(string) {
 }
 
 //Fonction qui converti un varInt -> décimal
-function convertVarIntToDeci(string) {
-
+function convertVarIntToDeci(varInt) {
+    let prefix = varInt.substring(0,2)
+    let tempDeci = ""
+    let width = 0
+    if (prefix == "fd") {
+        tempDeci = varInt.substring(2)
+        width = 4
+    } else if (prefix == "fe") {
+        tempDeci = varInt.substring(2)
+        width = 8
+    } else if (prefix == "ff") {
+        tempDeci = varInt.substring(2)
+        width = 16
+    } else {
+        tempDeci = varInt
+        width = 2
+    }
+    //controling input size
+    if (tempDeci.length != width) {
+        return "Please insert a varInt"
+    } else {
+        deci = parseInt(varInt, 16).toString()
+        return deci
+    }
 }
 
 //Fonction qui converti un varInt -> décimal
